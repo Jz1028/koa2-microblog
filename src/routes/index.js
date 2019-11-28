62db54a35c1e72dd415ac48263822689079a31db
jiangzhi <aposin@vip.qq.com>
2019-11-28 23:08:36 +0800

    feat:router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,30 @@
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
-
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
+	await ctx.render('index', {
+		title: 'Hello Koa 2!'
+	})
 })
 
 router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
+	ctx.body = {
+		title: 'koa2 json'
+	}
 })
+router.get('/profile/:username', async (ctx, next) => {
+	const {username} = ctx.params
+	ctx.body = {
+		title: 'this is a profile Api',
+		username}
+})
+
+router.get('/loadMore/:username/:pageIndex', async (ctx, next) => {
+	const {username,pageIndex} = ctx.params
+	ctx.body = {
+		title: 'this is a loadMore API',
+		username,pageIndex
+	}
+})
+
 
 module.exports = router
